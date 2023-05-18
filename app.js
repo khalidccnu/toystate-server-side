aabfb0e9df1b7d08172a8c265f94a6206b0f3a6e
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-05-18 13:34:09 +0600

    categories added

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+dotenv.config();
 app.use(cors());
+
+const mdbClient = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+(async (_) => {
+  try {
+    const categories = mdbClient.db("toystate").collection("categories");
+
+    app.get("/categories", async (req, res) => {
+      let result;
+
+      if (req.query.id) {
+        const query = { _id: new ObjectId(req.query.id) };
+        result = await categories.findOne(query);
+      } else {
+        const cursor = categories.find();
+        result = await cursor.toArray();
+      }
+
+      res.send(result);
+    });
+
+    mdbClient
+      .db("admin")
+      .command({ ping: 1 })
+      .then((_) => console.log("Successfully connected to MongoDB!"));
+  } catch (err) {
+    console.log("Did not connect to MongoDB! " + err.message);
+  } finally {
+    await mdbClient.close();
+  }
+})();
 
 app.get("/", (req, res) => {
   res.send("ToyState is running...");
